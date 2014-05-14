f6fdde843ff167a4b57c517cc9b284e33e91f1bb
Iain Mullan <iain.mullan@gmail.com>
2014-05-14 20:24:27 +0100

    added author note

--- swap.php
+++ swap.php
@@ -1,25 +1,28 @@
-<?php
-	
+<?php	
 /**
 
- This is an example token swap service written
- in PHP. This is required by
- the iOS SDK to authenticate a user.
+	@author Iain Mullan (@iainmullan)
+	@date 14/05/2014
 
- The service requires the Sinatra gem be installed:
+	This is an example token swap service written in PHP. This is required by
+	the Spotify iOS SDK to authenticate a user.
 
- To run the service, enter your client ID, client
- secret and client callback URL below and place the file on a web server.
+	To run the service, enter your client ID, client
+	secret and client callback URL below and place the file on a web server.
 
- Pass the public URI of this script (eg. http://localhost/swap.php) to the
- token swap method in the iOS SDK:
+	Pass the full URL of this script (eg. http://localhost/swap.php) to the
+	token swap method in the iOS SDK:
 
- NSURL *swapServiceURL = [NSURL urlWithString:@"http://localhost/swap.php"];
+	NSURL *swapServiceURL = [NSURL urlWithString:@"http://localhost/swap.php"];
 
- -[SPAuth handleAuthCallbackWithTriggeredAuthURL:url
-                   tokenSwapServiceEndpointAtURL:swapServiceURL
-                                        callback:callback];
+	-[SPAuth handleAuthCallbackWithTriggeredAuthURL:url
+	               tokenSwapServiceEndpointAtURL:swapServiceURL
+	                                    callback:callback];
 
+	Note: For the beta 1 release of the iOS SDK Spotify provides the
+	below beta values you can use in your Token Exchange Service code; later,
+	these values will be invalidated and will need to be replaced by your
+	own unique values.
 
 */
 
